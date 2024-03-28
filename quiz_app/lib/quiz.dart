eec33feb76a2e9697e586329a0286255cb0591e6
Hong Thy Nguyen <hongthy3101@gmail.com>
2024-03-27 22:56:37 -0400

    created quiz page and draft questions pages

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_scren.dart';
 
 class Quiz extends StatefulWidget {
-  const Quiz ({super.key})
-  
+  const Quiz({super.key});
+
   @override
   State<Quiz> createState() {
     return _QuizState();
